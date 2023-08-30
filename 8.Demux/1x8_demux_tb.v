05ab02ffcb536e4983d7156c46e4fb95700152c8
EdigaSuseela407 <142253218+EdigaSuseela407@users.noreply.github.com>
2023-08-30 18:40:39 +0530

    1x8_demux_tb.v

---
+++ 8.Demux/1x8_demux_tb.v
@@ -0,0 +1,23 @@
+
+module demux_1x8_tb;
+reg [2:0]S;
+reg A;
+wire [7:0]Y;
+demux_1x8 uut(.y(Y), .a(A), .s(S));
+initial begin
+A=1;
+S=3'd5;
+#30;
+A=0;
+S=3'd1;
+#30;
+A=1;
+S=3'd1;
+#30;
+S=3'd6;
+#30;
+S=3'd0;
+#30;
+$finish;
+end
+endmodule
