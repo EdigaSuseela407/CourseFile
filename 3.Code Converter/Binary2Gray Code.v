ac8416b5a4b682bd0e6613ce59758c5ba748e090
EdigaSuseela407 <142253218+EdigaSuseela407@users.noreply.github.com>
2023-08-30 16:34:26 +0530

    Binary2Gray Code.v

---
+++ 3.Code Converter/Binary2Gray Code.v
@@ -0,0 +1,9 @@
+
+module Bi_to_gr_code(
+    input [3:0] b,     
+    output [3:0] g);
+ xor A1(g[0],b[1],b[0]); 
+ xor A2(g[1],b[2],b[1]); 
+ xor A3(g[2],b[3],b[2]);
+ buf A4(g[3],b[3]);
+endmodule
