48185cc111a98cc1eb162a75de7490e6260b3538
EdigaSuseela407 <142253218+EdigaSuseela407@users.noreply.github.com>
2023-08-31 13:58:37 +0530

    Gray_Counter_tb.v

---
+++ 12.Gray_Code counter/Gray_Counter_tb.v
@@ -0,0 +1,20 @@
+
+module Gray_counter_tb;
+parameter N = 4;
+reg clk,reset;
+wire [N-1:0] gray_count;
+Gray_counter dut(clk, reset, gray_count);
+initial begin
+clk= 1'b0;
+forever #5 clk= ~clk;
+end
+initial begin
+reset= 1'b1;
+#10;
+reset= 1'b0;
+end
+initial begin
+$monitor("\t\t counter: %d", gray_count);
+#175 $finish;
+end
+endmodule
