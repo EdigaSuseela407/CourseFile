fd9ee8bdaa6f0bed67fc60fe92829f53e56d8302
EdigaSuseela407 <142253218+EdigaSuseela407@users.noreply.github.com>
2023-08-31 14:03:45 +0530

    Ring_counter.v

---
+++ 14.Ring Counter/Ring_counter.v
@@ -0,0 +1,19 @@
+
+module Ring_counter_tb;
+reg clk;
+reg reset;
+wire [3:0] out;
+Ring_counter dut (
+    .clk(clk),
+    .reset(reset),
+    .out(out));
+initial begin
+    clk = 0;
+    reset = 1;
+    #10 reset = 0;
+    forever #10 clk = ~clk;
+end
+initial begin
+    #100 $finish;
+end
+endmodule
